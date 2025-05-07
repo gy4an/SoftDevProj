84e8b25d378a7fcdb7639f5c7fc18240d1946866
DEEN ABDILLAH KHAYR ALELIS AQUINO <2120478@cc.usls.edu>
2025-05-07 15:23:35 +0800

    legend

---
+++ src/main/java/org/example/person.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class person {
+    String fn, ln;
+    public person(String fn, String ln){
+        this.fn = fn;
+        this.ln = ln;
+    }
+
+    public String getfn(){
+        return fn;
+    }
+
+    public String getln(){
+        return ln;
+    }
+}
