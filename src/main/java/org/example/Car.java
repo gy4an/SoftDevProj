b800eaaf29faf1b5fd4d49d6f577fb3a72345e5a
JOHN GABRIEL BASON BERGAVERA <2100498@cc.usls.edu>
2025-05-07 15:43:09 +0800

    Hello

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Car
+{
+    String firstName, lastName;
+
+    public Car
+            (String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
